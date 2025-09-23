56156308a709b1b03b5aa9772bad0cf36486aed8
raghu <raghuneshram@gmail.com>
2025-09-23 22:24:47 +0530

    First program

---
+++ 1.1 HelloWorld.py
@@ -0,0 +1,9 @@
+my_age = 22               # Assign an integer value to my_age
+half_my_age = my_age / 2  # Divide my_age by 2 for half_my_age
+greeting = "Hello"        # Assign a greeting string
+name = "Alex"             # Assign a name
+greeting_with_name = greeting + ", " + name + "!"  # Concatenate greeting and name
+print(greeting_with_name)
+print(my_age)
+
+
